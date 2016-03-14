4ac3c2d52fdecb62738332dfe57b16a4ffa18c60
michaelsuzukisagi <michael.suzuki@alfresco.com>
2016-03-14 15:54:54 +0000

    replaced static data with api driven data

--- app/components/charts/MainChart.js
+++ app/components/charts/MainChart.js
@@ -27,139 +27,11 @@ class MainChart extends React.Component{
     // //     });
     // //
     //
-    var mockdata =
-    [
-    {
-    date: "16/11/2015",
-    plannedDefectReduction: 44,
-    actualDefectReduction: 51,
-    planTestRun: -56,
-    testNotRun: -55.7
-    },
-    {
-    date: "17/11/2015",
-    plannedDefectReduction: 41,
-    actualDefectReduction: 52,
-    planTestRun: -52,
-    testNotRun: -52.43
-    },
-    {
-    date: "18/11/2015",
-    plannedDefectReduction: 39,
-    actualDefectReduction: 59,
-    planTestRun: -49,
-    testNotRun: -49.63
-    },
-    {
-    date: "19/11/2015",
-    plannedDefectReduction: 36,
-    actualDefectReduction: 59,
-    planTestRun: -46,
-    testNotRun: -37.91
-    },
-    {
-    date: "20/11/2015",
-    plannedDefectReduction: 34,
-    actualDefectReduction: 36,
-    planTestRun: -43,
-    testNotRun: -33.53
-    },
-    {
-    date: "23/11/2015",
-    plannedDefectReduction: 31,
-    actualDefectReduction: 31,
-    planTestRun: -39,
-    testNotRun: -28.69
-    },
-    {
-    date: "24/11/2015",
-    plannedDefectReduction: 29,
-    actualDefectReduction: 32,
-    planTestRun: -36,
-    testNotRun: -26.12
-    },
-    {
-    date: "25/11/2015",
-    plannedDefectReduction: 26,
-    actualDefectReduction: 35,
-    planTestRun: -33,
-    testNotRun: -24.54
-    },
-    {
-    date: "26/11/2015",
-    plannedDefectReduction: 23,
-    actualDefectReduction: 32,
-    planTestRun: -29,
-    testNotRun: -23.1
-    },
-    {
-    date: "27/11/2015",
-    plannedDefectReduction: 21,
-    actualDefectReduction: 23,
-    planTestRun: -26,
-    testNotRun: -22.51
-    },
-    {
-    date: "30/11/2015",
-    plannedDefectReduction: 18,
-    actualDefectReduction: 22,
-    planTestRun: -23,
-    testNotRun: -22.51
-    },
-    {
-    date: "1/12/2015",
-    plannedDefectReduction: 16,
-    actualDefectReduction: 26,
-    planTestRun: -20,
-    testNotRun: -12.16
-    },
-    {
-    date: "2/12/2015",
-    plannedDefectReduction: 13,
-    actualDefectReduction: 26,
-    planTestRun: -16,
-    testNotRun: -8.32
-    },
-    {
-    date: "3/12/2015",
-    plannedDefectReduction: 10,
-    actualDefectReduction: 27,
-    planTestRun: -13,
-    testNotRun: -5.93
-    },
-    {
-    date: "4/12/2015",
-    plannedDefectReduction: 8,
-    actualDefectReduction: 19,
-    planTestRun: -10,
-    testNotRun: -3.74
-    },
-    {
-    date: "7/12/2015",
-    plannedDefectReduction: 5,
-    actualDefectReduction: 10,
-    planTestRun: -7,
-    testNotRun: -2.84
-    },
-    {
-    date: "8/12/2015",
-    plannedDefectReduction: 3,
-    actualDefectReduction: 9,
-    planTestRun: -3,
-    testNotRun: -2.27
-    },
-    {
-    date: "9/12/2015",
-    plannedDefectReduction: 0,
-    actualDefectReduction: 9,
-    planTestRun: 0,
-    testNotRun: 0
-    }
-    ];
-    this.mountChart(mockdata);
+
+    this.mountChart();
 
     }
-    mountChart(dataInput){
+    mountChart(){
         var margin = {top: 20, right: 80, bottom: 30, left: 50},
             width = 960 - margin.left - margin.right,
             height = 500 - margin.top - margin.bottom;
@@ -196,18 +68,15 @@ class MainChart extends React.Component{
             .attr("height", height + margin.top + margin.bottom)
             .append("g")
             .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-            dataInput.forEach(function(d) {
-                d.date = parseDate(d.date);
-            });
-            // d3.json('reporting/api/alfresco/5.1/summary', function(error, data) {
-            //     if (error) throw error;
-            dataInput.map(function(data){
-
+            d3.json('http://localhost:3000/reporting/api/alfresco/5.1/summary', function(error, data) {
+                if (error) throw error;
                 color.domain(d3.keys(data[0]).filter(function(key) {
                     return key !== "date";
                 }));
 
-
+                data.forEach(function(d) {
+                    d.date = parseDate(d.date);
+                });
 
                 var lines = color.domain().map(function(name) {
                 return {
