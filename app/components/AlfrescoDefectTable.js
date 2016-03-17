4ae7f68a6aa84e1917e38dcd577f02c5e2470673
michaelsuzukisagi <michael.suzuki@alfresco.com>
2016-03-17 21:00:14 +0000

    Updated static tables to be data driven

---
+++ app/components/AlfrescoDefectTable.js
@@ -0,0 +1,47 @@
+import React from 'react'
+/**
+ * Usage: <Table data={} title="Section title"/>
+ * Takes in:
+ * String title for section title.
+ * Data array of json.
+ */
+export default class AlfrescoDefectTable extends React.Component{
+    constructor(props){
+        super(props)
+    }
+
+    render(){
+        console.log(this.props.data)
+        return(
+            <div>
+            <h2 className="sub-header">{this.props.title}</h2>
+            <div className="table-responsive">
+              <table className="table table-striped">
+                <thead>
+                  <tr>
+                    <th>Date</th>
+                    <th>Planned Defect Reduction</th>
+                    <th>Actual Defect Reduction</th>
+                    <th>Plan Test Run</th>
+                    <th>Actual Tests not yet run</th>
+                  </tr>
+                </thead>
+                <tbody>
+                    {this.props.data.map(function(x){
+                        return(
+                            <tr>
+                                <th>{x.date}</th>
+                                <th>{x.plannedDefectReduction}</th>
+                                <th>{x.actualDefectReduction}</th>
+                                <th>{x.planTestRun}</th>
+                                <th>{x.testNotRun}</th>
+                            </tr>)
+                    }
+                )}
+                </tbody>
+              </table>
+            </div>
+        </div>
+        )
+    }
+}
--- app/pages/Dashboard.js
+++ app/pages/Dashboard.js
@@ -3,20 +3,37 @@ import Navigation from '../components/nav/Navigation'
 import SideNavigation from '../components/nav/SideNavigation'
 import ContentLayout from '../layout/ContentLayout'
 import MainChart from '../components/charts/MainChart'
-import Table from '../components/Table'
-
+import Table from '../components/AlfrescoDefectTable'
+import Superagent from 'superagent'
 /**
  * Main entry point to applicaiton.
  * Author: Michael Suzuki
  */
-class App extends React.Component {
-  render () {
-    return (
-        <ContentLayout title="Dashboard">
-            <MainChart url="http://localhost:3000/reporting/api/alfresco/5.1/summary"/>
-            <Table title="Data"></Table>
-        </ContentLayout>
-    )
-  }
+export default class Dashboard extends React.Component {
+
+    constructor(props){
+        super(props)
+        this.state = {
+            data : [],
+            url : "http://localhost:3000/reporting/api/alfresco/5.1/summary"
+        }
+    }
+    componentDidMount(){
+        this.getDashboardData()
+    }
+
+    getDashboardData(result){
+        Superagent.get(this.state.url).then((res) => {
+            this.setState({ data:res.body})}
+        )
+    }
+    render () {
+
+        return (
+            <ContentLayout title="Dashboard">
+                <MainChart url={this.state.url}/>
+                <Table title="Data" data={this.state.data}></Table>
+            </ContentLayout>
+        )
+    }
 }
-export default App
