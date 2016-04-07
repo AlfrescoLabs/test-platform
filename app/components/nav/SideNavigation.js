6404463ae4c51cae8ce63405967fcb401819d81b
michaelsuzukisagi <michael.suzuki@alfresco.com>
2016-04-07 17:13:49 +0100

    sub nav now creates the link dynamically based on project name and version

--- app/components/nav/SideNavigation.js
+++ app/components/nav/SideNavigation.js
@@ -2,21 +2,45 @@ import React from 'react'
 import {Link, browserHistory} from 'react-router'
 
 class SideNavigation extends React.Component {
+
     constructor(props){
         super(props)
+        this.state = {
+            project:"",
+            version:""
+        }
     }
+    componentDidMount() {
+        this.updateState()
+    }
+
+    updateState() {
+        browserHistory.listen((ev)=> {
+            let paths = ev.hash.split("/")
+            this.setState({
+                "project" : paths[2],
+                "version" : paths[3].split("?")[0]
+            })
+        })
+    }
+
     render () {
+        let url = "project/" + this.state.project + "/" + this.state.version
+        let trend = url + "/defect/trend"
+        let defect = url + "/defect/discovery"
+        console.log("the url is:" + url)
+        console.log("trend url is:" + trend)
         return (
             <div className="col-sm-3 col-md-2 sidebar">
               <ul className="nav nav-sidebar">
                   <li>
-                      <Link activeClassName="active" to="/project/alfresco/5.1">Dashboard <span className="sr-only">(current)</span></Link>
+                      <Link activeClassName="active" to={url}>Dashboard <span className="sr-only">(current)</span></Link>
                   </li>
                   <li>
-                      <Link activeClassName="active" to="/project/alfresco/5.1/defect/discovery">Defect Discovery <span className="sr-only">(current)</span></Link>
+                      <Link activeClassName="active" to={defect}>Defect Discovery <span className="sr-only">(current)</span></Link>
                   </li>
                   <li>
-                      <Link activeClassName="active" to="/project/alfresco/5.1/defect/trend">Defect Trend <span className="sr-only">(current)</span></Link>
+                      <Link activeClassName="active" to={trend}>Defect Trend <span className="sr-only">(current)</span></Link>
                   </li>
                   {/*
                       <li><Link activeClassName="active" to="/perftest">Performance Tests</Link></li>
