5fbea0bda62550ffb22f777496dce79ef60c5377
michaelsuzukisagi <michael.suzuki@alfresco.com>
2016-05-06 17:51:33 +0100

    adding unit test and testing framework

---
+++ test/components/PageTitleTest.js
@@ -0,0 +1,7 @@
+import expect from 'expect'
+
+describe('Page title component to display title', ()=>{
+    it('it should display title hello when property is passed in',()=>{
+        expect(true).toEqual(true)
+    })
+})
